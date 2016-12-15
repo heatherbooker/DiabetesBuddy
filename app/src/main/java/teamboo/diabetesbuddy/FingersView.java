d0778b75154f0e7d0f8bdb9e84fdc59d5400ad17
Heather Booker <heather.booker@hotmail.com>
2016-12-15 16:39:50 -0500

    Initial commit

---
+++ app/src/main/java/teamboo/diabetesbuddy/FingersView.java
@@ -0,0 +1,13 @@
+package teamboo.diabetesbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FingersView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fingers_view);
+    }
+}
